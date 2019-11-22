7199c574faab452ef72c5ad7be9b8773a70e19a1
liliaze <diana.boudy@gmail.com>
2019-11-22 16:52:02 +0100

    Add login handler to login screen

--- src/view/login_screen.js
+++ src/view/login_screen.js
@@ -1,4 +1,5 @@
 import { createElement } from '../model/element.js';
+import { login } from '../controller/action.js';
 
 export function createLoginScreen() {
     const screen = createElement('div',
@@ -12,19 +13,31 @@ export function createLoginScreen() {
     return screen;
 }
 
+const usernameInputId = 'username_input';
+const passwordInputId = 'password_input';
+
 function createLoginForm() {
     const form = createElement('div', null, [
         createElement('div', null, [
             createElement('label', { for: 'username' }, 'Username:'),
-            createElement('input', { type: 'text', name: 'username', id: 'username_input', required: true, placeholder: 'medialis' }),
+            createElement('input', { type: 'text', name: 'username', id: usernameInputId, required: true, placeholder: 'medialis' }),
         ]),
         createElement('div', null, [
             createElement('label', { for: 'password' }, 'Password:'),
-            createElement('input', { type: 'password', name: 'password', id: 'password_input', required: true }),
+            createElement('input', { type: 'password', name: 'password', id: passwordInputId, required: true }),
         ]),
         createElement('button', { onclick: () => alert('TODO') }, 'New Account'),
-        createElement('button', { onclick: () => alert('TODO') }, 'Login'),
+        createElement('button', { onclick: handlerLoginClick }, 'Login'),
     ]);
 
     return form;
+}
+
+function handlerLoginClick(event) {
+    const username = document.getElementById(usernameInputId);
+    const password = document.getElementById(passwordInputId);
+
+    const token = login(username, password);
+
+    alert('You are logged in! Your token is: ' + token);
 }
