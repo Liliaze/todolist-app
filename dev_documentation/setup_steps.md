90914e4250f6aa63b121eb828848f30911e86fc1
liliaze <diana.boudy@gmail.com>
2019-11-22 10:10:00 +0100

    Setup project

---
+++ dev_documentation/setup_steps.md
@@ -0,0 +1,12 @@
+# commands
+
+```
+install node
+git init
+npm init
+npm i -D lite-server
+```
+
+* write .gitignore
+
+* project structure (directories and files)
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import {data} from './repository/action.js';
+
+window.addEventListener('load', main);
+
+function main() {
+    console.log('hello from main!', data);
+}
