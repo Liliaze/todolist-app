42b2bc980c0c4ed05d64be045b819a811caf211d
Liliaze <diana.boudy@gmail.com>
2019-11-29 15:37:47 +0100

    Add new features

--- src/controller/action.js
+++ src/controller/action.js
@@ -3,7 +3,7 @@ import LocalStorage from '../repository/local_storage.js';
 import { fetcher } from '../repository/fetcher.js';
 import { AccountQueries, TaskListQueries, TaskQueries } from '../repository/api_client.js';
 import { createLoginScreen, createTodoScreen, refreshTodoScreen } from '../view/index.js';
-import { refreshTaskListElement } from '../view/todo_screen.js';
+import { refreshTaskListElement, refreshTasksElement } from '../view/todo_screen.js';
 
 const { fetchJson } = fetcher(Config.ApiBaseUrl);
 
@@ -62,19 +62,88 @@ export async function addTaskList(title) {
   const jsonResult = await fetchJson(TaskListQueries.create(authToken, title));
 
   const { value: allTaskLists } = await fetchJson(TaskListQueries.getAll(authToken));
-  LocalStorage.replace('taskListCollection', allTaskLists);
+  LocalStorage.set('taskListCollection', allTaskLists);
+  refreshTaskListElement();
+}
+
+export async function deleteTaskList(taskListId) {
+  const authToken = getLocalAuthToken();
+
+  const jsonResult = await fetchJson(TaskListQueries.delete(authToken, taskListId));
+
+  const { value: allTaskLists } = await fetchJson(TaskListQueries.getAll(authToken));
+  LocalStorage.set('taskListCollection', allTaskLists);
+  refreshTaskListElement();
+}
+
+export async function addTask(taskListId, content) {
+  const authToken = getLocalAuthToken();
+
+  const jsonResult = await fetchJson(TaskQueries.create(authToken, taskListId, content));
+
+  await loadTasksCollection(authToken);
+  refreshTasksElement();
+}
+
+export async function deleteTask(taskId) {
+  const authToken = getLocalAuthToken();
+
+  const jsonResult = await fetchJson(TaskQueries.delete(authToken, taskId));
+
+  await loadTasksCollection(authToken);
+  refreshTasksElement();
+}
+
+export async function doneTask(taskId, content, status, taskListId) {
+  const authToken = getLocalAuthToken();
+
+  if (status === "active")
+    await fetchJson(TaskQueries.update(authToken, taskId, content, "done", taskListId));
+  else
+    await fetchJson(TaskQueries.update(authToken, taskId, content, "active", taskListId));
+
+  await loadTasksCollection(authToken);
+  refreshTasksElement();
+}
+
+export async function updateTask(taskId, content, status, taskListId) {
+  const authToken = getLocalAuthToken();
+
+  await fetchJson(TaskQueries.update(authToken, taskId, content, status, taskListId));
+
+  await loadTasksCollection(authToken);
+  refreshTasksElement();
+}
+
+export async function updateTaskList(title, taskListId) {
+  const authToken = getLocalAuthToken();
+
+  await fetchJson(TaskListQueries.update(authToken, taskListId, title));
+
+  await loadAllRemoteTasks();
   refreshTaskListElement();
 }
 
 export async function loadAllRemoteTasks() {
   const authToken = getLocalAuthToken();
 
+  await loadTaskListCollection(authToken);
+
+  await loadTasksCollection(authToken);
+}
+
+export async function loadTaskListCollection(authToken) {
   const { value: allTaskLists } = await fetchJson(TaskListQueries.getAll(authToken));
   LocalStorage.set('taskListCollection', allTaskLists);
+}
+
+export async function loadTasksCollection(authToken) {
+  const allTaskLists = getLocalTaskLists();
 
   const allTasksResults = await Promise.all(allTaskLists.map(taskList => getAllTasksFetch(authToken, taskList)));
   LocalStorage.set('taskCollection', allTasksResults);
 }
+
 
 async function getAllTasksFetch(authToken, taskList) {
   const { value: tasksResults } = await fetchJson(TaskQueries.getAll(authToken, taskList["tasklist_id"]));
