f6371089f0be87dd7169962b1d1ed029727a2ec5
liliaze <diana.boudy@gmail.com>
2019-12-06 22:40:53 +0100

    Refactor createElement to allow attributes to be a function with self element as param

--- src/model/element.js
+++ src/model/element.js
@@ -1,28 +1,64 @@
+// export function createElement(type, attributes, content) {
+//   const element = Object.assign(document.createElement(type), {
+//     ...(typeof attributes === 'function' ? attributes(element) : attributes), // TODO: to fix
+//     addContent: function (content) {
+//       if (content != null) {
+//         if (Array.isArray(content)) {
+//           content.forEach(element.addContent);
+//         } else if (content.appendChild) {
+//           element.appendChild(content);
+//         } else {
+//           const contentString = typeof content === 'object' ? JSON.stringify(content) : String(content);
+//           element.innerText += contentString;
+//         }
+//       }
+//       return element;
+//     },
+//     removeContent: function () {
+//       while (element.firstChild) {
+//         element.removeChild(element.firstChild);
+//       }
+//     },
+//     replaceContent: function (content) {
+//       element.removeContent();
+//       return element.addContent(content);
+//     }
+//   });
+//   return element.addContent(content);
+// }
+
 export function createElement(type, attributes, content) {
-  const element = Object.assign(document.createElement(type), {
-    ...attributes,
-    addContent: function (content) {
-      if (content != null) {
-        if (Array.isArray(content)) {
-          content.forEach(element.addContent);
-        } else if (content.appendChild) {
-          element.appendChild(content);
-        } else {
-          const contentString = typeof content === 'object' ? JSON.stringify(content) : String(content);
-          element.innerText += contentString;
-        }
+  const element = document.createElement(type);
+
+  element.addContent = content => {
+    if (content != null) {
+      if (Array.isArray(content)) {
+        content.forEach(element.addContent);
+      } else if (content.appendChild) {
+        element.appendChild(content);
+      } else {
+        const contentString = typeof content === 'object' ? JSON.stringify(content) : String(content);
+        element.innerText += contentString;
       }
-      return element;
-    },
-    removeContent: function () {
-      while (element.firstChild) {
-        element.removeChild(element.firstChild);
-      }
-    },
-    replaceContent: function (content) {
-      element.removeContent();
-      return element.addContent(content);
     }
-  });
+    return element;
+  };
+
+  element.removeContent = () => {
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
+    }
+  };
+
+  element.replaceContent = content => {
+    element.removeContent();
+    return element.addContent(content);
+  };
+
+  if (attributes != null) {
+    const attributeDict = typeof attributes === 'function' ? attributes(element) : attributes;
+    Object.keys(attributeDict).forEach(key => { element[key] = attributeDict[key]; });
+  }
+
   return element.addContent(content);
 }
